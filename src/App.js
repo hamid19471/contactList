8b0df8b0cb34db3c889e48f810305845a66a5bee
hamid asadi <hamidasadi@hamids-Mac-mini.local>
2022-09-20 21:14:43 +0430

    DELETE feature added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState } from "react";
 import Button from "./components/Buttons/SubmitButton";
 import Header from "./components/Header/Header";
 import Input from "./components/Input/Input";
 import Db from "./components/DB/Db";
 
 function App() {
+    const [contactList, setContactList] = useState([
+        {
+            id: 1,
+            firstName: "Hamid",
+            lastName: "Asadi",
+            phoneNumber: "09123456789",
+        },
+        {
+            id: 2,
+            firstName: "Ali",
+            lastName: "Golestani",
+            phoneNumber: "09351234567",
+        },
+        {
+            id: 3,
+            firstName: "Reza",
+            lastName: "Khosravi",
+            phoneNumber: "09194567890",
+        },
+    ]);
+    const deleteContact = (id) => {
+        setContactList(contactList.filter((item) => item.id !== id));
+    };
+
     return (
         <div className="container">
             <Header title="Contact List" />
@@ -23,7 +48,7 @@ function App() {
                 className="input-class"
             />
             <Button className="submit-button" title="Save Contact" />
-            <Db />
+            <Db onDelete={deleteContact} contactList={contactList} />
         </div>
     );
 }
--- src/components/DB/Db.jsx
+++ src/components/DB/Db.jsx
@@ -1,27 +1,7 @@
 import React from 'react'
 import './Db.style.css'
 import Button from '../Buttons/SubmitButton'
-const Db = () => {
-    const contactList =[
-        {
-            id:1,
-            firstName:'Hamid',
-            lastName:'Asadi',
-            phoneNumber:'09123456789',
-        },
-        {
-            id:2,
-            firstName:'Ali',
-            lastName:'Golestani',
-            phoneNumber:'09351234567',
-        },
-        {
-            id:3,
-            firstName:'Reza',
-            lastName:'Khosravi',
-            phoneNumber:'09194567890',
-        },
-    ]
+const Db = ({onDelete, contactList}) => {
   return (
     <div>
         {contactList.map((item)=>(
@@ -30,7 +10,7 @@ const Db = () => {
                     <h4>Last Name : {item.lastName}</h4>
                     <p>Phone Number : {item.phoneNumber}</p>
                     <div className='button-container'>
-                    <Button title='DELETE' className='delete-button'/>
+                    <Button title='DELETE' className='delete-button' onClick={()=>onDelete(item.id)}/>
                     <Button title='UPDATE' className='update-button'/>
                     </div>
             </div>
